2d8cb14e1777f505dd8a2d9b3f6ceeac7bb67c65
Gulam Faruk <golamfaruk204@gmail.com>
2021-11-09 07:20:59 +0600

    Modal Project Done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,73 @@
 $(document).ready(function() {
 
-  const formSubmit = function(){
-    $('.btn').click(function(){
-     
-  alert($('.alert-success').css({"display":"block"}))
-    });
+$('.popup').on("click", function (event) {
+  event.preventDefault();
+  $(this).hide();
+  $('.divMount').addClass('transparent');
+  $(".form-wrapper").css({'display': 'block'});
 
+})
+$('.timeBtn').on("click", function (event) {
+  event.preventDefault();
+  $('.divMount').removeClass('transparent');
+  $(".form-wrapper").css({'display': 'none'});
+  $('.popup').show();
+})
+$('.close').on("click", function (e) {
+  e.preventDefault();
+  $('.divMount').removeClass('transparent');
+  $(".form-wrapper").css({'display': 'none'});
+  $('.popup').show();
+})
+$('.save').on("click", function (e) {
+  e.preventDefault()
+ let confirmMassage = confirm("Successfully ...!!! Your Information Save .");
+setTimeout(function () {
+  if (confirmMassage === true) {
+    $('.divMount').removeClass('transparent');
+    $(".form-wrapper").css({'display': 'none'});
+    $('.popup').show();
   }
-  formSubmit()
+  else{
+    alert('Not Save Your Information...');
+    $(".form-wrapper").css({'display': 'none'});
+    $('.popup').show();
+  }
+}, 500);
+
+  // let transparent = $(".divMount");
+  // let popup = $(".popup");
+  // let formWrapper = $(".form-wrapper");
+
+  // $(transparent).on("click", function (params) {
+  //   console.log(html);
+  //   console.log(click);
+  //   console.log(params);
+  //   if (params.target == transparent && popup && formWrapper ) {
+  //     transparent.removeClass('transparent');
+  //     formWrapper.css({'display': 'none'});
+  //     popup.show();
+  //     $('.popup').show();
+
+  //   }
+  //   else{
+  //     alert("Don't Click Wrong Side ...")
+  //   }
+  // })
+
+})
+
 
 });
+
+
+    const transparent = document.querySelector('.divMount');
+    const popup = document.querySelector('.popup');
+    const dialogeBox = document.querySelector('.form-wrapper');
+    transparent.addEventListener("click", function (params) {
+      if (params.target == transparent && popup &&dialogeBox) {
+        transparent.style.display = "none";
+        popup.style.display = "block";
+        dialogeBox.style.display = "none";
+      }
+    })
